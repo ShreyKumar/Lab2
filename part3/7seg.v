2f070eabe8c7a578ff9bb92180a74df730a196dd
Andrew Blinn <me@disconcision.com>
2017-06-01 17:50:21 -0400

    started 7seg

---
+++ part3/7seg.v
@@ -0,0 +1,15 @@
+//SW[2:0] data inputs 
+//SW[9] select signal 
+//LEDR[0] output display
+
+module 7seg (LEDR, SW);
+	input[9:0] SW;
+	output[6:0] HEX0;
+
+	assign HEX0[0] = ~SW[3] & ~SW[2] & ~SW[1] &  SW[4] |
+			 ~SW[3] &  SW[2] & ~SW[1] & ~SW[4] |
+			  SW[3] &  SW[2] & ~SW[1] &  SW[4] |
+			  SW[3] & ~SW[2] &  SW[1] &  SW[4]
+endmodule
+
+
